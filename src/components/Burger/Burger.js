317a07068c0e3ecd48267913a518137a869dd4e8
hao.hung <hao.hung@hy-tech.com.tw>
2018-05-29 17:44:59 +0800

    Ouputting Burger Ingrediants Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css';
 import BugerIngrediant from './BurgerIngrediant/BurgerIngrediant';
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BugerIngrediant key={igKey + i} type={igKey} />;
+            });
+        });
+
     return (
         <div className={classes.Burger}>
-            <BugerIngrediant type="bread-top"/>
-            <BugerIngrediant type="cheese"/>
-            <BugerIngrediant type="meat"/>
-            <BugerIngrediant type="bread-bottom"/>
+            <BugerIngrediant type="bread-top" />
+            {transformedIngredients}
+            <BugerIngrediant type="bread-bottom" />
         </div>
     );
 };
